ccacd280f9a755602a97da281d7b6a05183deb1b
kakaJuli <jkovalenok@mail.ru>
2022-10-14 23:47:27 +0300

    сделала третье задание с прямоугольником

--- code.js
+++ code.js
@@ -3,11 +3,23 @@ let v = Math.pow(a, 3)
 alert(v);*/
 
 
-function paralepiped () {
+/* function paralepiped () {
     let s = prompt ("Введите площадь основания?");
     let h = prompt ("введите высоту?");
     let v = s * h;
     alert (v);
 }
 
-paralepiped();
+paralepiped(); */
+
+
+let a = prompt ("Введите значение стороны");
+let b = prompt ("Введите значение другой стороны?");
+let c = prompt("Введите значение стороны");
+
+function pramaugolnik () {
+    v =  a * b * c;
+    alert (v);
+}
+
+pramaugolnik ();
