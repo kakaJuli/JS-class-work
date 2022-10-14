59ba9e663749cd463158c4c54324162defd5aeb7
kakaJuli <jkovalenok@mail.ru>
2022-10-14 23:30:43 +0300

    добавила второе задание с паралепипедом

--- code.js
+++ code.js
@@ -1,3 +1,13 @@
-let a = prompt ("сторона куба?");
+/*let a = prompt ("сторона куба?");
 let v = Math.pow(a, 3)
-alert(v);
+alert(v);*/
+
+
+function paralepiped () {
+    let s = prompt ("Введите площадь основания?");
+    let h = prompt ("введите высоту?");
+    let v = s * h;
+    alert (v);
+}
+
+paralepiped();
